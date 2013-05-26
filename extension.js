e72f60ea5201ff9c8ec09d7c76da1c2f6b7ed180
Thomas Wendt <thoemy@gmx.net>
2013-05-27 00:22:13 +0200

    Left and right scrolling with some code restructuring.

--- extension.js
+++ extension.js
@@ -10,25 +10,20 @@
 // You should have received a copy of the GNU General Public License along with
 // this file. If not, see <http://www.gnu.org/licenses/>.
 
-const Gdk = imports.gi.Gdk;
-const Gio = imports.gi.Gio;
-const Gtk = imports.gi.Gtk;
-const GObject = imports.gi.GObject;
 const Gettext = imports.gettext.domain('gnome-shell-extensions');
 const _ = Gettext.gettext;
 
 const Clutter = imports.gi.Clutter
 const Lang = imports.lang
 const Main = imports.ui.main
-const Mainloop = imports.mainloop
 const Meta = imports.gi.Meta
-const WorkspaceSwitcherPopup= imports.ui.workspaceSwitcherPopup
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-let settings;
+let settings = null;
+let desktopscroller = null;
 
 const ScrollPosition = {
     RIGHT: 1,
@@ -39,9 +34,7 @@ const ScrollPosition = {
 
 const SCROLL_POSITION  = 'scroll-position';
 const SCROLL_DELAY = 'scroll-delay';
-
 const SCROLL_WIDTH = 1;
-var waiter_ms	= 0;
 
 function Scroller() {
     this._init();
@@ -49,121 +42,153 @@ function Scroller() {
 
 Scroller.prototype = {
     _init : function() {
+        this.actors = [];
 
-	this.actor = new Clutter.Rectangle({
-	    name: 'scroller',
-	    reactive: true
-	})
+	var monitors = Main.layoutManager.monitors;
 
-	var monitor = Main.layoutManager.primaryMonitor;
+        var max_right = -Infinity;
+        var min_left = Infinity;
+        var left_monitor = null;
+        var right_monitor = null;
 
-	var width = SCROLL_WIDTH;
-	var height = monitor.height - 30;
-	var x = monitor.width - width;
-	var y = 30;
-
-        switch (settings.get_enum(SCROLL_POSITION)) {
-	case ScrollPosition.RIGHT:
-	    width = SCROLL_WIDTH;
-	    height = monitor.height - 30;
-	    x = monitor.width - width;
-	    y = 30;
-	    break;
-
-	case ScrollPosition.LEFT:
-	    width = SCROLL_WIDTH;
-	    height = monitor.height - 30;
-	    x = 0;
-	    y = 30;
-	    break;
-
-	case ScrollPosition.TOP:
-	    width = monitor.width-20;
-	    height = SCROLL_WIDTH;
-	    x = 20;
-	    y = 0;
-	    break;
-
-	case ScrollPosition.BOTTOM:
-	    width = monitor.width;
-	    height = SCROLL_WIDTH;
-	    x = 0;
-	    y = monitor.height - height;
-	    break;
-
-	}
-
-
-	this.actor.set_position(x,y);
-	this.actor.set_width(width);
-	this.actor.set_height(height);
-	this.actor.opacity = 0;
-
-	this.actor._delegate = this;
-	this._timeoutId = null;
-
-	this.actor.connect('scroll-event', function(actor, event) {
-	    if (!actor._delegate._timeoutId) {
-		// see js/ui/workspacesView.js:1042
-		switch (event.get_scroll_direction()) {
-		case Clutter.ScrollDirection.UP:
-		    actor._delegate._doScroll(Meta.MotionDirection.UP);
-		    break;
-		case Clutter.ScrollDirection.DOWN:
-		    actor._delegate._doScroll(Meta.MotionDirection.DOWN);
-		    break;
-		}
-	    }
-	})
-
-	Main.layoutManager.addChrome(this.actor, { /* visibleInFullscreen:true */ });
-    },
-
-    _doScroll: function(direction) {
-	let newWs = Main.wm.actionMoveWorkspace(direction);
-
-	// for scrolling more than one time, but not at the same time if Delay Time is checked in extension preferences.
-	if(!settings.get_boolean(SCROLL_DELAY)) {
-	    waiter_ms = 0;
-        } else {
-            waiter_ms = 465;
+        /* Find the left-most and right-most monitor */
+        for(var i=0; i<monitors.length; i++) {
+            var left = monitors[i].x;
+            var right = monitors[i].x + monitors[i].width;
+            if(left < min_left) {
+                min_left = left;
+                left_monitor = monitors[i];
+            }
+            if(right > max_right) {
+                max_right = right;
+                right_monitor = monitors[i];
+            }
         }
 
-	this._timeoutId = Mainloop.timeout_add(waiter_ms, Lang.bind(this, function() { this._timeoutId = null; return false }));
+        if(!left_monitor || !right_monitor) {
 
-	// workspaceSwitcher popup, see js/ui/windowManager.js:575
-	if (!Main.overview.visible) {
-	    if (Main.wm._workspaceSwitcherPopup == null) {
-		Main.wm._workspaceSwitcherPopup = new WorkspaceSwitcherPopup.WorkspaceSwitcherPopup();
-		Main.wm._workspaceSwitcherPopup.connect('destroy', function() { Main.wm._workspaceSwitcherPopup = null });
-	    }
-	    // The 'workspace-switcher-group' takes all the screen, we should be on top to scroll more than one time
-	    Main.layoutManager.removeChrome(this.actor);
-	    Main.layoutManager.addChrome(this.actor, { /* visibleInFullscreen:true */ });
+        }
 
-	    Main.wm._workspaceSwitcherPopup.display(direction, newWs.index());
-	}
+        var actor = this._createScrollArea(left_monitor, ScrollPosition.LEFT);
+        this._addActor(actor);
+        actor = this._createScrollArea(right_monitor, ScrollPosition.RIGHT);
+        this._addActor(actor);
     },
 
-    destroy: function() {
-	if (this._timeoutId)
-            Mainloop.source_remove(this._timeoutId);
-	this._timeoutId = null;
+    _createScrollArea: function(monitor, type) {
+        var y_offset = 0;
+        /* Make sure we don't go over the panel */
+        if(monitor == Main.layoutManager.primaryMonitor)
+            y_offset = Main.panel.actor.get_height() + Main.panel._leftCorner.actor.get_height();
 
-	Main.layoutManager.removeChrome(this.actor);
-	this.actor.destroy();
+        /* Default values for the left side */
+	var x = 0;
+	var y = y_offset;
+	var width = SCROLL_WIDTH;
+	var height = monitor.height - y;
+
+        switch (type) {
+	case ScrollPosition.LEFT:
+            break;
+
+	case ScrollPosition.RIGHT:
+	    x = monitor.x + monitor.width - width;
+	    break;
+	}
+
+	var actor = new Clutter.Rectangle({
+	    name: 'scroller_' + type,
+	    reactive: true,
+            opacity: 0,
+            x: x,
+            y: y,
+            height: height,
+            width: width
+	});
+
+        return actor;
+    },
+
+    _addActor: function(actor) {
+        actor.connect('scroll-event', Lang.bind(this, this._onScrollEventSwitcher));
+	Main.layoutManager.addChrome(actor, { /* visibleInFullscreen:true */ });
+        this.actors.push(actor);
+    },
+
+    /**
+     * Switches workspaces and shows the switcher overlay.
+     */
+    _onScrollEventSwitcher: function(actor, event) {
+	switch (event.get_scroll_direction()) {
+	case Clutter.ScrollDirection.UP:
+            this._showWorkspaceSwitcher('switch-to-workspace-up');
+            return true;
+	case Clutter.ScrollDirection.DOWN:
+            this._showWorkspaceSwitcher('switch-to-workspace-down');
+	    return true;
+	}
+        return false;
+    },
+
+    /**
+     * Switches workspaces without showing the switcher overlay.
+     * Code taken from js/ui/workspacesView.js:724
+     */
+    _onScrollEventSafe: function(actor, event) {
+        switch (event.get_scroll_direction()) {
+        case Clutter.ScrollDirection.UP:
+            Main.wm.actionMoveWorkspace(Meta.MotionDirection.UP);
+            return true;
+        case Clutter.ScrollDirection.DOWN:
+            Main.wm.actionMoveWorkspace(Meta.MotionDirection.DOWN);
+            return true;
+        }
+        return false;
+    },
+
+    /**
+     * Switches worspaces and shows the workspace switcher overlay. Uses
+     * non-public functions and may stop working.
+     */
+    _showWorkspaceSwitcher: function(binding_str) {
+        let binding_obj = {
+            get_name: function() {
+                return binding_str;
+            }
+        }
+
+        /* Shows the switcher and scrolls */
+        Main.wm._showWorkspaceSwitcher(null, global.screen, null, binding_obj);
+
+        /* The workspace switcher is on top our scroll actors. Move them
+           to the top. */
+        for(let i=0; i<this.actors.length; i++) {
+            this.actors[i].raise_top();
+        }
+    },
+
+    /**
+     * Destroys the Scroller and its actors.
+     */
+    destroy: function() {
+        for(var i=0; i<this.actors.length; i++) {
+            var actor = this.actors[i];
+	    Main.layoutManager.removeChrome(actor);
+	    actor.destroy();
+        }
+        this.actors = null;
     }
 }
 
-let desktopscroller
 function init() {
     Convenience.initTranslations();
     settings = Convenience.getSettings();
 }
+
 function enable() {
-    settings = Convenience.getSettings();
     desktopscroller = new Scroller();
 }
+
 function disable() {
     desktopscroller.destroy();
     desktopscroller = null;
